dd9c2c1537e333dda468d26407bc8dffb1498ab0
christianrosado <christianrosado@haipriority.com>
2023-09-28 14:18:09 -0400

    adding first version to production

---
+++ .firebase/rewwardy/hosting/_next/static/blel2NQqjNx05eYb2IQ8R/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/bbb84dbed44cf4b3.css","static/chunks/pages/index-8068ddf10b831fcc.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
