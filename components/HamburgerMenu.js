5c19f91d754aeb8273a8adf4a16eca3ef2665229
Songiemar Garcia <39283072+sxngie@users.noreply.github.com>
2023-11-15 21:17:58 -0400

    Revert "Added a button to log out inside the menu."

--- components/HamburgerMenu.js
+++ components/HamburgerMenu.js
@@ -11,6 +11,24 @@ import ListItemText from '@mui/material/ListItemText';
 import MenuIcon from '@mui/icons-material/Menu';
 import Link from 'next/link';
 
+
+/*const HamburgerMenu = () => (
+    <div className={styles.shapingBar}>
+        <Menu customBurgerIcon={<HamburgerIcon />} width={'auto'} className={styles.burgermenubar}>
+            <div className={styles.listOfItems}>
+                <Link href="/dashboard" className={styles.menuItems}>Home</Link>
+                <Link href="/reward" className={styles.menuItems}>Rewards</Link>
+                <Link href="/challenge" className={styles.menuItems}>Challenges</Link>
+                <Link href="/scanner" className={styles.menuItems}>QR Scanner</Link>
+            </div>
+        </Menu>
+    </div>)
+
+const HamburgerIcon = () => (<div className='p-1/2'><svg fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="white"><path d="M4 6h16M4 12h16M4 18h16"></path></svg></div>)
+
+
+export default HamburgerMenu*/
+
 const HamburgerMenu = () => {
     const [isDrawerOpen, setDrawerOpen] = useState(false);
 
@@ -23,7 +41,7 @@ const HamburgerMenu = () => {
 
     return (
       <div>
-        <AppBar  sx={{ bgcolor: '#9B2C6B', mb: 1}}>
+        <AppBar sx={{ bgcolor: '#9B2C6B' }}>
           <Toolbar>
           <div style={{ flexGrow: 1 }} />
             <IconButton
@@ -58,11 +76,7 @@ const HamburgerMenu = () => {
                 <ListItemText primary="QR Scanner" sx={{ color: '#9B2C6B' }}/>
               </Link>
             </ListItem>
-            <ListItem button onClick={toggleDrawer(false)}>
-              <Link href="/">
-                <ListItemText primary="Sign out" sx={{ color: '#9B2C6B' }}/>
-              </Link>
-            </ListItem>
+            {/* Repeat for each link*/}
           </List>
         </Drawer>
       </div>
