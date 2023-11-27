5056abbf008d1adf5d4c548b307190be9334b8c6
sxngie <songiemar.garcia@upr.edu>
2023-11-27 17:27:26 -0400

    Removed spacegrotesk font usage across app

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -5,13 +5,13 @@ import { TopNavbar, AdminFooter } from "../../components";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { Button, TextField } from "@mui/material";
-import { Space_Grotesk } from "next/font/google";
+import { Inter } from "next/font/google";
 import { setCookie } from "cookies-next";
 import { db } from "@/firebase";
 import { collection, query, where, getDocs, limit } from "firebase/firestore";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-const space_grotesk = Space_Grotesk({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function AdminLogin() {
   const [email, setEmail] = useState("");
@@ -56,7 +56,7 @@ export default function AdminLogin() {
       </Head>
       {/* <TopNavbar/> */}
       <main className={`${styles.main} `}>
-        <h1 className={`${styles.header} ${space_grotesk.className}`}>
+        <h1 className={`${styles.header}`}>
           Login Business
         </h1>
         <div className={styles.form}>
