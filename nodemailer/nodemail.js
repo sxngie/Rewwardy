b8dc2d41ca973237ab67bdb9e4674517ef50c716
emiliodejesus <emilio.dejesus@upr.edu>
2023-11-30 13:13:40 -0400

    Added email notification templates!

---
+++ nodemailer/nodemail.js
@@ -0,0 +1,8 @@
+var transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "a7e9e1384babb3",
+      pass: "e55d6a3a23806a"
+    }
+  });
